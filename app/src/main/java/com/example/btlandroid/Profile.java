4a03155e6ce6dfc77993eece1116d97c818572d9
Xuân Kiên <kienlovend@gmail.com>
2021-05-29 21:37:22 +0700

    update realtime db

--- app/src/main/java/com/example/btlandroid/Profile.java
+++ app/src/main/java/com/example/btlandroid/Profile.java
@@ -11,6 +11,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 import com.bumptech.glide.Glide;
@@ -19,17 +20,28 @@ import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class Profile extends AppCompatActivity {
 
 
     TextView name, mail;
-    Button logout;
+    TextInputLayout fullName,email,phoneNo,password;
+    Button logout,update;
     ImageView img;
     Uri photoUrl;
     private GoogleSignInClient mGoogleSignInClient;
+    String _USERNAME, _EMAIL, _PHONENO, _PASSWORD, _NAME;
+
+    DatabaseReference reference;
 
 
     @Override
@@ -38,10 +50,21 @@ public class Profile extends AppCompatActivity {
         setContentView(R.layout.activity_profile);
 
 
+        reference = FirebaseDatabase.getInstance().getReference("users");
+
+
         logout = findViewById(R.id.logout);
         name = findViewById(R.id.name);
         mail = findViewById(R.id.mail);
         img = (ImageView) findViewById(R.id.profile_image);
+
+
+        fullName = findViewById(R.id.full_name_profile);
+        email = findViewById(R.id.email_profile);
+        phoneNo = findViewById(R.id.phone_no_profile);
+        password = findViewById(R.id.password_profile);
+        update = findViewById(R.id.btnUpdate);
+
 
 
         GoogleSignInAccount signInAccount = GoogleSignIn.getLastSignedInAccount(this);
@@ -54,6 +77,7 @@ public class Profile extends AppCompatActivity {
                     .load(photoUrl)
                     .centerCrop()
                     .into(img);
+//            fullName.setTe(signInAccount.getDisplayName());
         }
 
 
@@ -76,7 +100,66 @@ public class Profile extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        update.setOnClickListener(v -> {
+            update();
+        });
+
+        showAllUserData();
+    }
+
+    private void showAllUserData(){
+        Intent intent = getIntent();
+        _USERNAME = intent.getStringExtra("username");
+        _NAME = intent.getStringExtra("name");
+        _EMAIL = intent.getStringExtra("email");
+        _PHONENO = intent.getStringExtra("phoneNo");
+        _PASSWORD = intent.getStringExtra("password");
+
+        fullName.getEditText().setText(_NAME);
+        email.getEditText().setText(_EMAIL);
+        phoneNo.getEditText().setText(_PHONENO);
+        password.getEditText().setText(_PASSWORD);
+
+
+        
+
+    }
+
+    public void update(){
+
+//        if(isNameChanged() || isPasswordChanged()){
+            Map<String,Object> mapValue = new HashMap<>();
+            mapValue.put("email", email.getEditText().getText().toString());
+            mapValue.put("name",name.getText().toString());
+            mapValue.put("password",password.getEditText().getText().toString());
+            mapValue.put("phoneNo",phoneNo.getEditText().getText().toString());
+            mapValue.put("username","username");
+            DatabaseReference dbf = reference.child("testUpdate");
+            dbf.updateChildren(mapValue);
+//            Toast.makeText(this,"Data has been updated", Toast.LENGTH_LONG).show();
+//        }
+//        else Toast.makeText(this,"Data ise same and can not be update", Toast.LENGTH_LONG).show();
 
 
     }
+
+    private boolean isNameChanged() {
+        if( !_NAME.equals(fullName.getEditText().getText().toString())){
+            reference.child("testUpdate").child("name").setValue(fullName.getEditText().getText().toString());
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    private boolean isPasswordChanged() {
+        if( !_PASSWORD.equals(password.getEditText().getText().toString())){
+            reference.child("testUpdate").child("password").setValue(password.getEditText().getText().toString());
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+
 }
