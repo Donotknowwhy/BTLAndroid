1f1fed96198900cc3d6d975914a29dd87bbbf5aa
Xuân Kiên <kienlovend@gmail.com>
2021-05-30 13:13:10 +0700

    update img profile

--- app/src/main/java/com/example/btlandroid/MainActivity.java
+++ app/src/main/java/com/example/btlandroid/MainActivity.java
@@ -1,14 +1,20 @@
 package com.example.btlandroid;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -20,6 +26,8 @@ import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
@@ -33,7 +41,11 @@ public class MainActivity extends AppCompatActivity {
 
     private GoogleSignInClient mGoogleSignInClient;
     private final static int RC_SIGN_IN = 123;
-    private FirebaseAuth mAuth;
+    FirebaseAuth mAuth;
+    EditText mEmail,mPassword;
+    Button mLoginBtn;
+    TextView mCreateBtn,forgotTextLink;
+    ProgressBar progressBar;
 
 
     @Override
@@ -58,6 +70,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+        mEmail = findViewById(R.id.Email);
+        mPassword = findViewById(R.id.password);
+        progressBar = findViewById(R.id.progressBar);
+        mLoginBtn = findViewById(R.id.loginBtn);
+        mCreateBtn = findViewById(R.id.createText);
+        forgotTextLink = findViewById(R.id.forgotPassword);
+
         mAuth = FirebaseAuth.getInstance();
 
 
@@ -68,6 +87,98 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 signIn();
+            }
+        });
+
+        mLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String email = mEmail.getText().toString().trim();
+                String password = mPassword.getText().toString().trim();
+
+                if(TextUtils.isEmpty(email)){
+                    mEmail.setError("Email is Required.");
+                    return;
+                }
+
+                if(TextUtils.isEmpty(password)){
+                    mPassword.setError("Password is Required.");
+                    return;
+                }
+
+                if(password.length() < 6){
+                    mPassword.setError("Password Must be >= 6 Characters");
+                    return;
+                }
+
+                progressBar.setVisibility(View.VISIBLE);
+
+                // authenticate the user
+
+                mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(MainActivity.this, "Logged in Successfully", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                        }else {
+                            Toast.makeText(MainActivity.this, "Error ! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            progressBar.setVisibility(View.GONE);
+                        }
+
+                    }
+                });
+
+            }
+        });
+
+        mCreateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),Register.class));
+            }
+        });
+
+        forgotTextLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                final EditText resetMail = new EditText(v.getContext());
+                final AlertDialog.Builder passwordResetDialog = new AlertDialog.Builder(v.getContext());
+                passwordResetDialog.setTitle("Reset Password ?");
+                passwordResetDialog.setMessage("Enter Your Email To Received Reset Link.");
+                passwordResetDialog.setView(resetMail);
+
+                passwordResetDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // extract the email and send reset link
+                        String mail = resetMail.getText().toString();
+                        mAuth.sendPasswordResetEmail(mail).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Toast.makeText(MainActivity.this, "Reset Link Sent To Your Email.", Toast.LENGTH_SHORT).show();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(MainActivity.this, "Error ! Reset Link is Not Sent" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        });
+
+                    }
+                });
+
+                passwordResetDialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // close the dialog
+                    }
+                });
+
+                passwordResetDialog.create().show();
+
             }
         });
 
--- app/src/main/java/com/example/btlandroid/Profile.java
+++ app/src/main/java/com/example/btlandroid/Profile.java
@@ -1,14 +1,18 @@
 package com.example.btlandroid;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -20,11 +24,17 @@ import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.squareup.picasso.Picasso;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -32,26 +42,34 @@ import java.util.Map;
 
 public class Profile extends AppCompatActivity {
 
-
-    TextView name, mail;
+    private static final int GALLERY_INTENT_CODE = 1023 ;
+    TextView name, mail, fullName1,email1,phone1,verifyMsg1;
     TextInputLayout fullName,email,phoneNo,password;
-    Button logout,update;
+    Button logout,update, resetPassLocal,changeProfileImage;
     ImageView img;
+    FirebaseUser user;
+    FirebaseAuth mAuth;
     Uri photoUrl;
     private GoogleSignInClient mGoogleSignInClient;
     String _USERNAME, _EMAIL, _PHONENO, _PASSWORD, _NAME;
 
     DatabaseReference reference;
+    StorageReference storageReference;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        mAuth = FirebaseAuth.getInstance();
+        user = mAuth.getCurrentUser();
 
 
         reference = FirebaseDatabase.getInstance().getReference("users");
 
+
+        changeProfileImage = findViewById(R.id.changeProfile);
+        resetPassLocal = findViewById(R.id.resetPasswordLocal);
 
         logout = findViewById(R.id.logout);
         name = findViewById(R.id.name);
@@ -65,19 +83,39 @@ public class Profile extends AppCompatActivity {
         password = findViewById(R.id.password_profile);
         update = findViewById(R.id.btnUpdate);
 
+        storageReference = FirebaseStorage.getInstance().getReference();
 
 
-        GoogleSignInAccount signInAccount = GoogleSignIn.getLastSignedInAccount(this);
-        if(signInAccount != null){
-            name.setText(signInAccount.getDisplayName());
-            mail.setText(signInAccount.getEmail());
-            photoUrl = signInAccount.getPhotoUrl();
-            Glide
-                    .with(this)
-                    .load(photoUrl)
-                    .centerCrop()
-                    .into(img);
+
+        FirebaseUser user = mAuth.getCurrentUser();
+        if(user!=null){
+            StorageReference profileRef = storageReference.child("users/"+user.getUid()+"/profile.jpg");
+            profileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                @Override
+                public void onSuccess(Uri uri) {
+                    Log.d("AppLog",uri.toString());
+                    Picasso.get().load(uri).into(img);
+                }
+            });
+            name.setText(user.getDisplayName());
+            mail.setText(user.getEmail());
+
+        }else {
+
+            GoogleSignInAccount signInAccount = GoogleSignIn.getLastSignedInAccount(this);
+            if (signInAccount != null) {
+                name.setText(signInAccount.getDisplayName());
+                mail.setText(signInAccount.getEmail());
+                photoUrl = signInAccount.getPhotoUrl();
+                Glide
+                        .with(this)
+                        .load(photoUrl)
+                        .centerCrop()
+                        .into(img);
+
+                Log.d("url", String.valueOf(photoUrl));
 //            fullName.setTe(signInAccount.getDisplayName());
+            }
         }
 
 
@@ -104,62 +142,81 @@ public class Profile extends AppCompatActivity {
             update();
         });
 
-        showAllUserData();
+
+
+        changeProfileImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // open gallery
+                Intent i = new Intent(v.getContext(),EditProfile.class);
+            i.putExtra("fullName", name.getText().toString());
+            i.putExtra("email", mail.getText().toString());
+            i.putExtra("phone","1234");
+                startActivity(i);
+            }
+        });
+
+        resetPassLocal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                final EditText resetPassword = new EditText(v.getContext());
+
+                final AlertDialog.Builder passwordResetDialog = new AlertDialog.Builder(v.getContext());
+                passwordResetDialog.setTitle("Reset Password ?");
+                passwordResetDialog.setMessage("Enter New Password > 6 Characters long.");
+                passwordResetDialog.setView(resetPassword);
+
+                passwordResetDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // extract the email and send reset link
+                        Log.d("user", String.valueOf(user));
+                        String newPassword = resetPassword.getText().toString();
+                        user.updatePassword(newPassword).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Toast.makeText(Profile.this, "Password Reset Successfully.", Toast.LENGTH_SHORT).show();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(Profile.this, "Password Reset Failed.", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                });
+
+                passwordResetDialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // close
+                    }
+                });
+
+                passwordResetDialog.create().show();
+
+            }
+        });
+
     }
 
-    private void showAllUserData(){
-        Intent intent = getIntent();
-        _USERNAME = intent.getStringExtra("username");
-        _NAME = intent.getStringExtra("name");
-        _EMAIL = intent.getStringExtra("email");
-        _PHONENO = intent.getStringExtra("phoneNo");
-        _PASSWORD = intent.getStringExtra("password");
-
-        fullName.getEditText().setText(_NAME);
-        email.getEditText().setText(_EMAIL);
-        phoneNo.getEditText().setText(_PHONENO);
-        password.getEditText().setText(_PASSWORD);
 
 
-        
 
-    }
 
-    public void update(){
+    public void update() {
 
-//        if(isNameChanged() || isPasswordChanged()){
-            Map<String,Object> mapValue = new HashMap<>();
-            mapValue.put("email", email.getEditText().getText().toString());
-            mapValue.put("name",name.getText().toString());
-            mapValue.put("password",password.getEditText().getText().toString());
-            mapValue.put("phoneNo",phoneNo.getEditText().getText().toString());
-            mapValue.put("username","username");
-            DatabaseReference dbf = reference.child("testUpdate");
-            dbf.updateChildren(mapValue);
-//            Toast.makeText(this,"Data has been updated", Toast.LENGTH_LONG).show();
-//        }
-//        else Toast.makeText(this,"Data ise same and can not be update", Toast.LENGTH_LONG).show();
+
+        Map<String, Object> mapValue = new HashMap<>();
+        mapValue.put("email", email.getEditText().getText().toString());
+        mapValue.put("name", name.getText().toString());
+        mapValue.put("password", password.getEditText().getText().toString());
+        mapValue.put("phoneNo", phoneNo.getEditText().getText().toString());
+        mapValue.put("username", "username");
+        DatabaseReference dbf = reference.child("testUpdate");
+        dbf.updateChildren(mapValue);
 
 
     }
-
-    private boolean isNameChanged() {
-        if( !_NAME.equals(fullName.getEditText().getText().toString())){
-            reference.child("testUpdate").child("name").setValue(fullName.getEditText().getText().toString());
-            return true;
-        }else{
-            return false;
-        }
-    }
-
-    private boolean isPasswordChanged() {
-        if( !_PASSWORD.equals(password.getEditText().getText().toString())){
-            reference.child("testUpdate").child("password").setValue(password.getEditText().getText().toString());
-            return true;
-        }else{
-            return false;
-        }
-    }
-
-
 }
